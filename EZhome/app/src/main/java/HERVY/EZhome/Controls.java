012b3603900bc095a8793446cb494f86e2ac3bc2
ErickCantos13 <31775210+ErickCantos13@users.noreply.github.com>
2017-10-05 20:47:12 -0400

    Uploading Project Files

---
+++ EZhome/app/src/main/java/HERVY/EZhome/Controls.java
@@ -0,0 +1,21 @@
+package HERVY.EZhome;
+
+/**
+ * Created by Owner123 on 10/4/2017.
+ */
+
+public class Controls {
+
+    public static void adjLights(){
+        // Controls brightness and light state
+    }
+    public static void adjTemp(int temp){
+        // Adjust temperature from user input
+    }
+    public static void lightsTimer(int time){
+        // Timer for lights
+    }
+    public static void tempTimer(int time){
+        // Timer for temperature
+    }
+}
